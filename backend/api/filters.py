c9a9e5f5de1c97c8c2560e392944c4c1ee1df0d2
Andrey Dubov <andrey123829@yandex.ru>
2026-04-14 18:57:05 +0300

    is_staff

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,9 +1,11 @@
 import django_filters as filters
+
 from recipe.models import Recipe
 
 
 class RecipeFilter(filters.FilterSet):
     """Фильтрация по нескольким тегам."""
+
     tags = filters.CharFilter(method='filter_tags')
     author = filters.NumberFilter(field_name='author')
 
@@ -12,14 +14,10 @@ class RecipeFilter(filters.FilterSet):
         fields = ('tags',)
 
     def filter_tags(self, queryset, name, value):
+        """Возвращает QuerySet с наличием хотя бы одного из тегов в рецепте."""
         tags = self.request.GET.getlist('tags')
 
         if not tags:
             return queryset
 
         return queryset.filter(tags__slug__in=tags).distinct()
-
-        # for tag in tags:
-        #     queryset = queryset.filter(tags__slug=tag)
-
-        # return queryset.distinct()
--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,10 +5,12 @@ class AuthorOrReadOnly(permissions.BasePermission):
     """Доступ к редактированию только автору или для чтения."""
 
     def has_permission(self, request, view):
+        """Доступ к редактированию только авторизированным пользователям."""
         return (
             request.method in permissions.SAFE_METHODS
             or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.author
+        """Разрешение на редактирование только автору или администратору."""
+        return request.user == obj.author or request.user.is_staff
