c329f5b728620512a6d6312cdc49d07974341923
Andrey Dubov <andrey123829@yandex.ru>
2026-04-17 13:51:56 +0300

    Two review

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = [
     'foodgram.serveblog.net', '158.160.209.12', 'localhost', '127.0.0.1'
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 #     }
 # }
 
-if DEBUG:
+if bool(DEBUG):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
