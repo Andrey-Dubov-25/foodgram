4ccc97c77cb853525394e9a9443e2f04ac4d91e3
Andrey Dubov <andrey123829@yandex.ru>
2026-04-18 18:10:34 +0300

    Short-link

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -10,7 +10,11 @@ from api.views import ShortLinkView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api)),
-    path('s/<str:short_link>/', ShortLinkView.as_view())
+    path(
+        's/<str:short_link>/',
+        ShortLinkView.as_view(),
+        name='redirect_short_link'
+    )
 ]
 
 
