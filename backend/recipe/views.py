f866df5f26aa6b1e16cb485fbd7da8d1c6068d19
Andrey Dubov <andrey123829@yandex.ru>
2026-04-18 13:12:26 +0300

    Short-link

--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import redirect
 
 def recipe_for_short_link(request, pk):
     """Возвращает страницу рецепта по короткой ссылке."""
-    return redirect(f'recipe/{pk}/')
+    return redirect(f'recipes/{pk}/')
