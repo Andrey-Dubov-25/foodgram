1b602cb4bf2d1792a49de17acf72ed08d3ac6072
Andrey Dubov <andrey123829@yandex.ru>
2026-04-18 17:23:47 +0300

    Short-link

--- backend/recipe/views.py
+++
@@ -1,9 +0,0 @@
-# from django.shortcuts import get_object_or_404, redirect
-
-# from .models import Recipe
-
-
-# def redirect_short_link(request, short_link):
-#     """Перенаправление по короткой ссылке на рецепт."""
-#     recipe = get_object_or_404(Recipe, short_link=short_link)
-#     return redirect(f'/recipes/{recipe.pk}/')
